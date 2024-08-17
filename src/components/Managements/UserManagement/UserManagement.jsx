2e520ab6a56f608a41e85d3185d2321834dd6e2a
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-08-17 20:04:02 +0700

    Update icon to svg, and edit class

--- src/components/Managements/UserManagement/UserManagement.jsx
+++ src/components/Managements/UserManagement/UserManagement.jsx
@@ -1,25 +1,25 @@
 import "./UserManagement.scss"
-import searchIcon from "../../../assets/icons/search.png"
+import searchIcon from "../../../assets/icons/search.svg"
 import addIcon from "../../../assets/icons/Add.svg"
-import editIcon from "../../../assets/icons/edit.png"
-import deleteIcon from "../../../assets/icons/delete.png"
+import editIcon from "../../../assets/icons/edit.svg"
+import deleteIcon from "../../../assets/icons/delete.svg"
 
 const UserManagement = () => {
 
     return(
-        <div className="userMana-container">
-            <h1 className="pageName-header">user management</h1>
-            <div className="userName-header">
-                <div className="searchUser">
-                    <input className="searchInput" type="text" placeholder="Enter search information"/>
-                    <img className="searchIcon" src={searchIcon} alt="Search" />
+        <div className="user-mana-container">
+            <h1 className="user-name-header">user management</h1>
+            <div className="user-header">
+                <div className="user-header-search">
+                    <input className="search-input" type="text" placeholder="Enter search information"/>
+                    <img className="search-icon" src={searchIcon} alt="Search" />
                 </div>
-                <button className="addUser">
+                <button className="btn btn-primary">
                     Create User
                     <img src={addIcon} alt="Add" />
                 </button>
             </div>
-            <div className="userMana-body">
+            <div className="user-mana-body">
                 <table>
                     <tr>
                         <th>ID</th>
@@ -34,10 +34,10 @@ const UserManagement = () => {
                         <td>VanANguyen@gmail.com</td>
                         <td>Docter</td>
                         <td>
-                            <button className="editIcon">
+                            <button className="edit-icon">
                                 <img src={editIcon} alt="Edit" />
                             </button>
-                            <button className="deteleIcon">
+                            <button className="detele-icon">
                                 <img src={deleteIcon} alt="Delete" />
                             </button>
                         </td>
@@ -48,10 +48,10 @@ const UserManagement = () => {
                         <td>VanBNguyen@gmail.com</td>
                         <td>Docter</td>
                         <td>
-                            <button className="editIcon">
+                            <button className="edit-icon">
                                 <img src={editIcon} alt="Edit" />
                             </button>
-                            <button className="deteleIcon">
+                            <button className="detele-icon">
                                 <img src={deleteIcon} alt="Delete" />
                             </button>
                         </td>
@@ -62,10 +62,10 @@ const UserManagement = () => {
                         <td>VanCNguyen@gmail.com</td>
                         <td>Patient</td>
                         <td>
-                            <button className="editIcon">
+                            <button className="edit-icon">
                                 <img src={editIcon} alt="Edit" />
                             </button>
-                            <button className="deteleIcon">
+                            <button className="detele-icon">
                                 <img src={deleteIcon} alt="Delete" />
                             </button>
                         </td>
@@ -76,10 +76,10 @@ const UserManagement = () => {
                         <td>VanDNguyen@gmail.com</td>
                         <td>Patient</td>
                         <td>
-                            <button className="editIcon">
+                            <button className="edit-icon">
                                 <img src={editIcon} alt="Edit" />
                             </button>
-                            <button className="deteleIcon">
+                            <button className="detele-icon">
                                 <img src={deleteIcon} alt="Delete" />
                             </button>
                         </td>
