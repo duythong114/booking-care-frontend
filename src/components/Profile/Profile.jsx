9ae98a72663c4f7bdbf9be461c5b32c9637ff5b4
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-08-18 14:20:17 +0700

    Push code profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,35 @@
+import "./Profile.scss"
+import avatar from "../../assets/icons/Avatar.svg"
+import editIcon from "../../assets/icons/edit.svg"
+
 const Profile = () => {
     return (
         <div className="profile-container">
-            profile page
+            <h1 className="profile-name">personal information</h1>
+            <div className="profile-name-infor">
+                <div className="infor-avatar">
+                    <img className="infor-img-avatar" src={avatar} alt="Avatar" />  
+                    <img className="infor-img-edit" src={editIcon} alt="Edit" />
+                </div>
+                <div className="info-fields">
+                    <span>Nguyen Van A</span>
+                </div>
+                <div className="info-fields">
+                    <span>dd/mm/yyyy</span>
+                </div>
+                <div className="info-fields">
+                    <span>Male</span>
+                </div>
+                <div className="info-fields">
+                    <span>0932359483</span>
+                </div>
+                <div className="info-fields">
+                    <span>VanANguyen@gmail.com</span>
+                </div>
+                <div className="info-fields">
+                    <span>BinhThanh, HoChiMinh</span>
+                </div>
+            </div>
         </div>
     )
 }
