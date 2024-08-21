2d304fc4e8f15b28e3058077d63ad086b937aa19
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-08-21 08:54:39 +0700

    update code fc

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,37 @@
 import "./Profile.scss"
 import avatar from "../../assets/icons/Avatar.svg"
 import editIcon from "../../assets/icons/edit.svg"
+import { useSelector } from "react-redux"
+
 
 const Profile = () => {
+    const userInfo = useSelector(state => state.user.userInfo)
+
     return (
         <div className="profile-container">
             <h1 className="profile-name">Profile</h1>
             <div className="profile-name-infor">
                 <div className="infor-avatar">
-                    <img className="infor-img-avatar" src={avatar} alt="Avatar" />  
+                    <img className="infor-img-avatar" src={userInfo?.avatarUrl ? userInfo.avatarUrl : avatar}use alt="Avatar" />  
                     <img className="infor-img-edit" src={editIcon} alt="Edit" />
                 </div>
                 <div className="info-fields">
-                    <span>Nguyen Van A</span>
+                    <span>{userInfo.fullName}</span>
                 </div>
                 <div className="info-fields">
-                    <span>dd/mm/yyyy</span>
+                    <span>{userInfo.dob}</span>
                 </div>
                 <div className="info-fields">
-                    <span>Male</span>
+                    <span>{userInfo.gender}</span>
                 </div>
                 <div className="info-fields">
-                    <span>0932359483</span>
+                    <span>{userInfo.phone}</span>
                 </div>
                 <div className="info-fields">
-                    <span>VanANguyen@gmail.com</span>
+                    <span>{userInfo.email}</span>
                 </div>
                 <div className="info-fields">
-                    <span>BinhThanh, HoChiMinh</span>
+                    <span>{userInfo.address}</span>
                 </div>
             </div>
         </div>
