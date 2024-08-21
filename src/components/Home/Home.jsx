367da4f4a20b7febee0c87601e2290b31ccd8901
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-08-21 16:23:11 +0700

    update code home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,61 +1,35 @@
 import "./Home.scss"
 import addIcon from "../../assets/icons/Add.svg"
-import { useSelector } from "react-redux"
+import Bottles from "../../assets/images/blmedicin.jpg"
+import Blender from "../../assets/images/heath.jpg"
+import Pills from "../../assets/images/medicin.jpg"
+import BlisterPack from "../../assets/images/medicins.jpg"
+import Microscope from "../../assets/images/research.jpg"
 
 const Home = () => {
-    const userInfo = useSelector(state => state.user.userInfo)
-
     return (
         <div className="home-container">
             <div className="home-body-header">
                 <div className="body-header-name">
-                    <h1 className="home-body-name">personal information</h1>
                     <button className="btn btn-primary">
                         Booking
                         <img src={addIcon} alt="Add" />
                     </button>
                 </div>
-
-                <div className="body-header-table">
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>{userInfo?.fullName}</td>
-                                <td>{userInfo?.dob}</td>
-                                <td>{userInfo?.gender}</td>
-                            </tr>
-                            <tr>
-                                <td>{userInfo?.email}</td>
-                                <td>{userInfo?.phone}</td>
-                                <td>{userInfo?.address}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
             </div>
 
             <div className="home-body-infor">
-                <h1 className="body-infor-name">medical record</h1>
-                
-                <div className="body-infor-medical">
-                    <div className="infor-medical">
-                        <span><strong>Record ID:</strong>001</span>
-                    </div>
-                    <div className="infor-medical">
-                        <span><strong>Doctor:</strong>Nguyen Van A</span>
-                    </div>
-                    <div className="infor-medical">
-                        <span><strong>Date:</strong>dd/mm/yyyy</span>
-                    </div>
-                    <div className="infor-medical">
-                        <span><strong>Status:</strong>Pending</span>
-                    </div>
-                    <div className="infor-medical">
-                        <span><strong>Diagnosis:</strong>Cold with cough and mild fever</span>
-                    </div>
-                    <div className="infor-medical">
-                        <span><strong>Treatment:</strong>Paracetamol 500mg, twice daily. Rest and drink plenty of fluids</span>
-                    </div>
+                <div className="body-panel">
+                    <p className="body-panel-text">
+                        MeriCal is a <strong>Premier Private Label</strong> and <strong>Contract Manufacturer</strong> in the United States.
+                    </p>
+                </div>
+                <div className="panel-item">
+                    <img src={Bottles} alt="Bottles" />
+                    <img src={Blender} alt="Blender" />
+                    <img src={Pills} alt="Pills" />
+                    <img src={BlisterPack} alt="BlisterPack" />
+                    <img src={Microscope} alt="Microscope" />
                 </div>
             </div>
         </div>
